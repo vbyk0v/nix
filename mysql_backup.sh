8cea7cf9661fe57c2a2e136bc4f3b48f5790eaff
vbyk0v <34297043+vbyk0v@users.noreply.github.com>
2017-12-06 10:00:07 +0300

    Update mysql_backup.sh

--- mysql_backup.sh
+++ mysql_backup.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-/usr/bin/mysqldump -uroot -p1qaz@WSX3edc testlink > /home/user/backup/$(date +%Y-%m-%d-%H.%M.%S)_backup.sql
+/usr/bin/mysqldump -uroot -p12345 testlink > /home/user/backup/$(date +%Y-%m-%d-%H.%M.%S)_backup.sql
